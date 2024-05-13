1a002220c2a1d2b7d8ff02803171d7c0420abf8b
pornoiya <lactnovpidor@gmail.com>
2024-05-13 18:36:45 +0200

    Popup reveal

--- api.js
+++ api.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 function checkEmail(email) {
     return String(email)
     .toLowerCase()
@@ -6,9 +8,26 @@ function checkEmail(email) {
 
 export function onSumbit() {
     const emailField = document.getElementById("launch-form-email");
+    const successPopup = document.getElementById("screen-launch-form_popup-success");
+    const emailPopup = document.getElementById("screen-launch-form_popup-email");
+    const errorPopup = document.getElementById("screen-launch-form_popup-error");
     const email = emailField?.value;
 
+    const body = document.getElementsByTagName("body")?.[0];
+
     if (checkEmail(email)) {
-        console.log("OK LERS GOO");
+        // axios.put()
+        body.classList.add("show-popup"); 
+        successPopup.classList.remove("hidden");
     }
+}
+
+export function onPopupClose() {
+    const body = document.getElementsByTagName("body")?.[0];
+    const popups = document.getElementsByClassName("screen-launch-form_popup");
+    for (let cross of popups) {
+        cross.classList.add("hidden");
+    }
+    body.classList.remove("show-popup");
+
 }
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import { toggleCustomerSeller } from "./content-toggle"
-import { onSumbit } from "./api"
+import { onSumbit, onPopupClose } from "./api"
+
 toggleCustomerSeller("chip-instructions__sellers", "chip-instructions__customers",
 "instructions__sellers", "instructions__customers",
 "chip-customer-seller-toggle__checked", "hidden"
@@ -11,4 +12,11 @@ toggleCustomerSeller("chip-faq__sellers", "chip-faq__customers",
 );
 
 const submitButton = document.getElementById("launch-form-submit-btn");
+
 submitButton.addEventListener('click', onSumbit)
+
+const popupCrossButton = document.getElementsByClassName("screen-launch-form_popup__header-cross-wrapper");
+
+for (let btn of popupCrossButton) {
+    btn.addEventListener('click', onPopupClose)
+}
